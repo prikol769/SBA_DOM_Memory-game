617a1b762cf05878c95ed0fd5e9b2a19c521a287
Mykyta Krumalenko <prikol769@gmail.com>
2024-06-17 18:58:31 -0500

    added btn new game

--- app.js
+++ app.js
@@ -37,70 +37,111 @@ const arrayCardsContent = [
 let cardsChosen = [];
 let cardOneName = "";
 let cardTwoName = "";
+let countdownSec = 3;
 
 const content = document.getElementById("content");
-let cards;
+
 content.style.display = "grid";
 content.style.gridTemplateColumns = "150px 150px 150px 150px";
 content.style.gap = "16px";
 content.style.justifyContent = "center";
 
-for (let i = 0; i < arrayCardsContent.length; i++) {
-  const card = document.createElement("div");
-  card.textContent = arrayCardsContent[i];
-  card.setAttribute("name", `${arrayCardsContent[i]}`);
+const createBoardHandler = () => {
+  arrayCardsContent.sort(() => 0.5 - Math.random());
 
-  card.style.boxShadow =
-    "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
-  card.style.borderRadius = "8px";
-  card.style.width = "150px";
-  card.style.height = "150px";
-  card.style.cursor = "pointer";
-  card.style.fontSize = "100px";
-  card.style.display = "flex";
-  card.style.justifyContent = "center";
-  card.style.alignItems = "center";
-  card.style.color = "rgb(255, 255, 255)";
-  card.style.transition = "1s";
+  for (let i = 0; i < arrayCardsContent.length; i++) {
+    const card = document.createElement("div");
+    card.textContent = arrayCardsContent[i];
+    card.setAttribute("name", `${arrayCardsContent[i]}`);
 
-  card.addEventListener("click", (e) => {
-    const currentCardName = e.target.getAttribute("name");
+    card.style.boxShadow =
+      "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
+    card.style.borderRadius = "8px";
+    card.style.width = "150px";
+    card.style.height = "150px";
+    card.style.cursor = "pointer";
+    card.style.fontSize = "100px";
+    card.style.display = "flex";
+    card.style.justifyContent = "center";
+    card.style.alignItems = "center";
+    card.style.color = "rgb(255, 255, 255)";
+    card.style.transition = "1s";
 
-    if (cardsChosen.find((cardName) => cardName === currentCardName)) {
-      return;
-    }
+    card.addEventListener("click", (e) => {
+      const currentCardName = e.target.getAttribute("name");
 
-    const isNotOpenCard = e.target.style.color === "rgb(255, 255, 255)";
-    e.target.style.color = "#000";
-
-    if (!cardOneName && isNotOpenCard) {
-      cardOneName = currentCardName;
-    } else if (cardOneName && isNotOpenCard) {
-      cardTwoName = currentCardName;
-      if (cardOneName === cardTwoName) {
-        cardsChosen.push(cardOneName, cardTwoName);
-      } else {
-        const arrCardOneName = document.getElementsByName(cardOneName);
-        const arrCardTwoName = document.getElementsByName(cardTwoName);
-
-        console.log("arrCardTwoName", arrCardTwoName);
-
-        setTimeout(() => {
-          arrCardOneName[0].style.color = "rgb(255, 255, 255)";
-          arrCardOneName[1].style.color = "rgb(255, 255, 255)";
-          arrCardTwoName[0].style.color = "rgb(255, 255, 255)";
-          arrCardTwoName[1].style.color = "rgb(255, 255, 255)";
-        }, 300);
+      if (cardsChosen.find((cardName) => cardName === currentCardName)) {
+        return;
       }
 
-      cardOneName = "";
-      cardTwoName = "";
+      const isNotOpenCard = e.target.style.color === "rgb(255, 255, 255)";
+      e.target.style.color = "#000";
+
+      if (!cardOneName && isNotOpenCard) {
+        cardOneName = currentCardName;
+      } else if (cardOneName && isNotOpenCard) {
+        cardTwoName = currentCardName;
+        if (cardOneName === cardTwoName) {
+          cardsChosen.push(cardOneName, cardTwoName);
+        } else {
+          const arrCardOneName = document.getElementsByName(cardOneName);
+          const arrCardTwoName = document.getElementsByName(cardTwoName);
+
+          console.log("arrCardTwoName", arrCardTwoName);
+
+          setTimeout(() => {
+            arrCardOneName[0].style.color = "rgb(255, 255, 255)";
+            arrCardOneName[1].style.color = "rgb(255, 255, 255)";
+            arrCardTwoName[0].style.color = "rgb(255, 255, 255)";
+            arrCardTwoName[1].style.color = "rgb(255, 255, 255)";
+          }, 300);
+        }
+
+        cardOneName = "";
+        cardTwoName = "";
+      }
+    });
+    content.appendChild(card);
+  }
+};
+
+const btnStart = document.getElementById("start-btn");
+
+const handleStartGame = () => {
+  while (content.hasChildNodes()) {
+    content.removeChild(content.firstChild);
+  }
+
+  createBoardHandler();
+
+  cardsChosen = [];
+  countdownSec = 3;
+  cardOneName = "";
+  cardTwoName = "";
+
+  btnStart.disabled = true;
+
+  const spanCountdown = document.getElementById("countdown");
+
+  let countdownTimer = setInterval(() => {
+    if (countdownSec <= 0) {
+      spanCountdown.textContent = "";
+      clearInterval(countdownTimer);
+      btnStart.disabled = false;
     }
+    spanCountdown.textContent = countdownSec <= 0 ? "" : countdownSec;
+    countdownSec -= 1;
+  }, 900);
 
-    console.log("cardsChosen", cardsChosen);
-    console.log("cardOneName", cardOneName);
-    console.log("cardTwoName", cardTwoName);
-  });
+  for (let i = 0; i < arrayCardsContent.length; i++) {
+    content.children[i].style.color = "#000";
+    content.style.pointerEvents = "none";
 
-  content.appendChild(card);
-}
+    setTimeout(() => {
+      content.children[i].style.color = "rgb(255, 255, 255)";
+      content.style.pointerEvents = "auto";
+    }, 3000);
+  }
+};
+
+btnStart.onclick = handleStartGame;
